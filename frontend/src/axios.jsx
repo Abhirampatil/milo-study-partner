01e4b6bf125950b5a9867e2286e0c3e51fe60fc1
Abhirampatil <abhirampatil2005@gmail.com>
2026-04-03 01:29:37 +0530

    Vercel Integration

--- frontend/src/axios.jsx
+++ frontend/src/axios.jsx
@@ -1,6 +1,8 @@
 import axios from 'axios'
 
-const API = axios.create({ baseURL: 'https://milo-study-partner.onrender.com' })
+const API = axios.create({ 
+  baseURL: import.meta.env.VITE_API_URL 
+})
 
 API.interceptors.request.use((req) => {
   const token = localStorage.getItem('token')
